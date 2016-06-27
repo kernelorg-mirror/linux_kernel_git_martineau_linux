4373d551d03725cf1a274a186b4d2c591c278132
Mat Martineau <mathew.j.martineau@linux.intel.com>
2016-06-27 16:45:16 -0700

    KEYS: Restrict key linkage using a specific keychain
    Adds restrict_link_by_signature_keyring(), which uses the restrict_key
    member of the provided destination_keyring data structure as the
    key or keyring to search for signing keys.
    
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>

--- crypto/asymmetric_keys/asymmetric_type.c
+++ crypto/asymmetric_keys/asymmetric_type.c
@@ -513,6 +513,11 @@ static struct key_restriction *asymmetric_restriction_alloc(
 	return keyres;
 }
 
+static void asymmetric_free_key_data(void *data)
+{
+	key_put((struct key *)data);
+}
+
 /*
  * look up keyring restrict functions for asymmetric keys
  */
@@ -528,6 +533,24 @@ static struct key_restriction *asymmetric_lookup_restrict(char *restriction)
 		return asymmetric_restriction_alloc(
 			restrict_link_by_builtin_and_secondary_trusted,
 			NULL, NULL);
+
+	restrict_method = strsep(&restriction, ":");
+
+	if ((strcmp(restrict_method, "key_or_keyring") == 0) && restriction) {
+		key_serial_t serial;
+		struct key *key;
+
+		if (kstrtos32(restriction, 0, &serial) < 0)
+			return ERR_PTR(-EINVAL);
+
+		key = key_lookup(serial);
+		if (IS_ERR(key))
+			return ERR_PTR(-EINVAL);
+
+		return asymmetric_restriction_alloc(restrict_link_by_key_or_keyring,
+						    asymmetric_free_key_data,
+						    key);
+	}
 
 	return ERR_PTR(-EINVAL);
 }
